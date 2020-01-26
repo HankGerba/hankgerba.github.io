2feda7b0e35a47f1c5a5567f84ec3887b6fa72f9
HankGerba <hankg@stanford.edu>
2020-01-25 17:19:25 -0800

    dumb mistake...

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 function setup() {
-  createCanvas(width, height);
+  createCanvas(400, 800);
   background(51);
 
 }
 
 function draw() {
   background(51);
+  fill(0);
   ellipse(mouseX, mouseY, 16, 16);
 
 
